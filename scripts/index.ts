2487cf1b9931b31cc1646da0d58fb3caeced5936
Vahagn <metavahagn@gmail.com>
2025-12-08 12:30:04 +0400

    fix: script

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,7 +2,7 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import chalk from "chalk";
-import * as Core from "../core/src/index";
+import * as Core from "../core/dist/index.mjs";
 
 const { IconStyle, icons } = Core;
 
